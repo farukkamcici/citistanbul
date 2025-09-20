b4a8465ad4e37586ab283cdcd8f5c13c048daf4b
Faruk Kamçıcı <farukkamcici@gmail.com>
2025-09-20 13:04:53 +0300

    rev: next app

--- frontend/components/DistrictLayer.tsx
+++ frontend/components/DistrictLayer.tsx
@@ -2,19 +2,19 @@
 
 import { useEffect, useState } from "react";
 import { Source, Layer } from "react-map-gl/maplibre";
+import type { FeatureCollection } from "geojson";
 
 export default function DistrictLayer() {
-  const [geojson, setGeojson] = useState<any>(null);
+  const [geojson, setGeojson] = useState<FeatureCollection | null>(null);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   useEffect(() => {
-    fetch("http://207.154.235.183:8000/districts")
-      .then((res) => res.json())
-      .then((resp) => {
-        // API response: {status: "success", data: {...FeatureCollection...}}
-        setGeojson(resp.data);
-      })
-      .catch((err) => console.error("District fetch error:", err));
-  }, []);
+      fetch(`${API_URL}/districts`)
+        .then((res) => res.json())
+        .then((resp) => setGeojson(resp.data))
+        .catch((err) => console.error("District fetch error:", err));
+    }, []);
+
 
   if (!geojson) return null;
 
