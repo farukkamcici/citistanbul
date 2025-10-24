76347c6ea26bcb2fc792e6297c16ba52a80250de
Faruk Kamçıcı <farukkamcici@gmail.com>
2025-10-24 16:00:06 +0300

    updated es.py

--- api/app/es.py
+++ api/app/es.py
@@ -1,4 +1,8 @@
+import os
+
 from elasticsearch import Elasticsearch
 
+
 def get_es_client():
-    return Elasticsearch("http://localhost:9200")
+    es_url = os.environ.get("ELASTIC_URL", "http://localhost:9200")
+    return Elasticsearch(es_url, sniff_on_start=False, sniff_on_connection_fail=False)
